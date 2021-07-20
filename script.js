a75a7079d50685760a81ab147937bdf393732adc
Rahul Tiwari <rahul.s.tiwari9@gmail.com>
2021-07-21 00:34:14 +0530

    task copleted script and css done

--- script.js
+++ script.js
@@ -6,33 +6,31 @@ let state = {
 }
 
 function firstRender() {
-  let x = document.createElement("div");
-  x.className = 'container'
-  document.body.appendChild(x);
-  x.style.overflowX = 'auto'
+  let container = document.createElement("div");
+  container.className = 'container'
+  document.body.appendChild(container);
+  container.style.overflowX = 'auto'
   let table = document.createElement("table");
   let pages = document.createElement("div");
   pages.className = 'pages'
-  document.getElementsByClassName('container')[0].append(table, pages);
+  container.append(table, pages);
   let thead = document.createElement("thead")
   let tbody = document.createElement("tbody")
   let trh = document.createElement("tr")
-  let trb = document.createElement("tr")
+  let trb1 = document.createElement("td")
   let th1 = document.createElement("th")
   let th2 = document.createElement("th")
   let th3 = document.createElement("th")
-  let trb1 = document.createElement("td")
   th1.innerText = "ID"
   th2.innerText = "Name"
   th3.innerText = "Email"
   trb1.setAttribute('colspan', 3)
   trb1.innerText = "Data loading..."
-  document.getElementsByTagName('table')[0].append(thead, tbody)
-  document.getElementsByTagName('thead')[0].append(trh)
-  document.getElementsByTagName('tr')[0].append(th1, th2, th3)
-  document.getElementsByTagName('tbody')[0].append(trb1)
+  table.append(thead, tbody)
+  thead.append(trh)
+  trh.append(th1, th2, th3)
+  tbody.append(trb1)
   getUserData();
-
 }
 firstRender();
 
@@ -42,37 +40,36 @@ async function getUserData() {
   let pageButtons = []
   state.tableData = data
   state.pages = Math.ceil(data.length / state.rows)
-  console.log(state.pages);
   let first = document.createElement("a")
   first.id = "firstButton"
   first.innerText = "First"
-  first.setAttribute('href','#')
-  first.setAttribute('onclick','paginate("1")')
+  first.setAttribute('href', 'javascript:void(0);')
+  first.setAttribute('onclick', 'paginate("1")')
   let previous = document.createElement("a")
   previous.id = "previousButton"
   previous.innerText = "Previous"
-  previous.setAttribute('href','#')
+  previous.setAttribute('href', 'javascript:void(0);')
   let next = document.createElement("a")
   next.id = "nextButton"
   next.innerText = "Next"
-  next.setAttribute('href','#')
+  next.setAttribute('href', 'javascript:void(0);')
   let last = document.createElement("a")
   last.id = "lastButton"
   last.innerText = "Last"
-  last.setAttribute('href','#')
+  last.setAttribute('href', 'javascript:void(0);')
   last.setAttribute('onclick', `paginate(${state.pages})`)
-  
-  
+
+
   for (let i = 1; i <= state.pages; i++) {
     let pagebutton = document.createElement("a")
     pagebutton.id = `page-${i}`
     pagebutton.innerText = `${i}`
-    pagebutton.setAttribute('href','#')
+    pagebutton.setAttribute('href', 'javascript:void(0);')
     pagebutton.setAttribute('onclick', `paginate(${i})`)
     pageButtons.push(pagebutton)
 
   }
-  document.getElementsByClassName('pages')[0].append(first,previous,...pageButtons,next,last)
+  document.getElementsByClassName('pages')[0].append(first, previous, ...pageButtons, next, last)
   paginate(state.page)
 }
 //getUserData()
@@ -88,22 +85,33 @@ async function paginate(page = 1) {
   page == 1 ? document.getElementById('previousButton').style.display = 'none' : document.getElementById('previousButton').style.display = 'unset';
   page == state.pages ? document.getElementById('nextButton').style.display = 'none' : document.getElementById('nextButton').style.display = 'unset';
   renderRows(trimmedData)
+  let buttons =  document.getElementsByTagName('a')
+  Array.from(buttons).forEach(element => {
+    element.classList.remove('active')
+    element.innerText ==state.page? element.classList.add('active'):null;
+  });
+
 }
 
 function renderRows(trimmedData) {
   let tbody = document.getElementsByTagName('tbody')[0]
   tbody.innerHTML = '';
+  let datarows = []
   trimmedData.forEach(element => {
-    tbody.innerHTML += `<tr>
-    <td>${element.id}</td>
-    <td>${element.name}</td>
-    <td>${element.email}</td>
-  </tr>`
+    let bodyrow = document.createElement("tr")
+    let th1 = document.createElement("th")
+    let th2 = document.createElement("th")
+    let th3 = document.createElement("th")
+    th1.innerText = `${element.id}`
+    th2.innerText = `${element.name}`
+    th3.innerText = `${element.email}`
+    bodyrow.append(th1, th2, th3)
+    datarows.push(bodyrow)
+    //   tbody.innerHTML += `<tr>
+    //   <td></td>
+    //   <td>${element.name}</td>
+    //   <td>${element.email}</td>
+    // </tr>`
   });
-  
-  // let trb = document.createElement("tr")
-  // let id = document.createElement("td")
-  // let name = document.createElement("td")
-  // let email = document.createElement("td")
-  // id.innerText = "ID"
+  tbody.append(...datarows)
 }
